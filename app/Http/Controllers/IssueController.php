c8e306f2a9a05f8b4ac55f9df3b7452a799281e7
gjf54 <trrt1004@gmail.com>
2024-04-01 19:08:06 +0300

    2024.04.01 Test

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -6,10 +6,10 @@ use App\Models\Issue;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Notifications\Issue as IssueNotify;
+use Illuminate\Support\Facades\Notification;
 
 class IssueController extends Controller
 {
-
     public function create_issue(Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
@@ -21,9 +21,10 @@ class IssueController extends Controller
         $notifiable = " ";
 
         $issue = Issue::create($request->all());
-        $issue->notify(new IssueNotify($notifiable));
-        
+        // $issue->notify(new IssueNotify($notifiable));
+        Notification::send($issue, new Notification());
 
         return back()->with('message', 'Issue successful created! We will send answer to Your e-mail.');
     }
+
 }
