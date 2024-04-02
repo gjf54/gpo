15086f0cf6ba12bf29a848ff7a983aaf595d70ec
gjf54 <trrt1004@gmail.com>
2024-04-02 19:55:30 +0300

    2024.04.02 Test

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -6,10 +6,12 @@ use App\Models\Issue;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Notifications\Issue as IssueNotify;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Notification;
 
 class IssueController extends Controller
 {
+
     public function create_issue(Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required',
@@ -18,11 +20,15 @@ class IssueController extends Controller
             'issue' => 'required',
         ])->validate();
 
-        $notifiable = " ";
-
-        $issue = Issue::create($request->all());
-        // $issue->notify(new IssueNotify($notifiable));
-        Notification::send(Issue::class, new IssueNotify($issue));
+        Http::post('https://discord.com/api/webhooks/1224763076394483844/FqAGxvwmfi1GGzE4Mf6gaEIrR4YRO91EVe817OQojHACEjMTGgQePXNyNj9R6DNTgQmq', [
+            'content' => 'There is new issue!',
+            'embeds' => [
+                [
+                    'title' => 'Issue from '. $request->name,
+                    'description' => $request->issue,
+                ]
+            ],
+        ]);
 
         return back()->with('message', 'Issue successful created! We will send answer to Your e-mail.');
     }
