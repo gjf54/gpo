fd8192a8fd48ba03b7a28d118555e78fb6c9be50
gjf54 <trrt1004@gmail.com>
2024-04-03 09:48:31 +0300

    2024.04.03 Issue notify update

--- app/Http/Controllers/IssueController.php
+++ app/Http/Controllers/IssueController.php
@@ -6,6 +6,7 @@ use App\Models\Issue;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use App\Notifications\Issue as IssueNotify;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Notification;
 
@@ -18,14 +19,24 @@ class IssueController extends Controller
             'email' => 'required|email',
             'topic' => 'required',
             'issue' => 'required',
-        ])->validate();
+        ])->validate(); 
 
         Http::post('https://discord.com/api/webhooks/1224763076394483844/FqAGxvwmfi1GGzE4Mf6gaEIrR4YRO91EVe817OQojHACEjMTGgQePXNyNj9R6DNTgQmq', [
             'content' => 'There is new issue!',
             'embeds' => [
                 [
-                    'title' => 'Issue from '. $request->name . ' ' . $request->last_name,
-                    'description' => $request->issue . '\n' . '**E-mail: '. $request->email .'**',
+                    'author' => [
+                        'name' => $request->name . ' ' . $request->last_name,
+                    ],
+                    'description' => $request->issue,
+                    'fields' => [
+                        'name' => 'E-mail',
+                        'value' => $request->email,
+                        'inline' => False,
+                    ],
+                    'footer' => [
+                        'text' => 'Date: '. Date::now(),
+                    ]
                 ]
             ],
         ]);
